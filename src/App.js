a18dc5518cfd22a19ea1e600579e01261cd0e45a
Yamil <yamilgiron98@gmail.com>
2024-02-02 05:58:37 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Amigurumi from "./Componentes/Amigurumis.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Amigurumi/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componentes/Amigurumis.js
@@ -0,0 +1,20 @@
+import react from "react";
+
+function Amigurumi(){
+  return(
+    <div className="contenedor-amigurumi">
+      <img 
+        className="imagen-amigurumi"
+        src={require("../imagenes/Logo.jpg")}
+        alt="imagen del amigurumi"/>
+      <div className="contenedor-texto-amigurumi">
+        <p className="nombre-amigurumi">Duky</p>
+        <p className="dificultad-amigurumi">Facil</p>
+        <p className="descripcion-amigurumi">Duky es un patito amarillo con muy buen humor, sabe muy bien como</p>
+        <p className="precio-amigurumi"></p>
+      </div> 
+    </div>
+  );
+};
+
+export default Amigurumi
